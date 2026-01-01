5caae2eb8570948fc63da2444120d8aba04a9ccf
Laraib Memon <laraibmemon.bssef22@iba-suk.edu.pk>
2026-01-01 22:34:47 +0500

    update problem75 Sort Colors

--- Problems1_100/Problem75_SortColors.py
+++ Problems1_100/Problem75_SortColors.py
@@ -1,17 +1,26 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-      l=0
-      r = len(nums) -1
-      i = 0
+        # l = left pointer (for placing 0s)
+        l = 0
+        # r = right pointer (for placing 2s)
+        r = len(nums) - 1
+        # i = current index we are scanning
+        i = 0
 
-      while i<=r:
-        if nums[i] == 0:
-            nums[i],nums[l]=nums[l],nums[i]
-            l+=1
-        elif nums[i] == 2: 
-            nums[i],nums[r] = nums[r],nums[i]
-            r-= 1
-            i -= 1
+        # Loop until current index crosses right pointer
+        while i <= r:
+            if nums[i] == 0:
+                # If current element is 0, swap it to the left side
+                nums[i], nums[l] = nums[l], nums[i]
+                l += 1
+            elif nums[i] == 2:
+                # If current element is 2, swap it to the right side
+                nums[i], nums[r] = nums[r], nums[i]
+                r -= 1
+                # Decrement i to re-check the swapped element
+                i -= 1
 
-        i+=1
+            # Move to the next element
+            i += 1
+
         # No return needed, as the list is sorted in place
